213c33a587a3b6a47859a9d43f82c3c6ec674f00
Thomas Dameron <tdameron1@cnm.edu>
2020-01-28 10:39:24 -0700

    Attempting to create the constructor and mutator methods.

--- php/Classes/Author.php
+++ php/Classes/Author.php
@@ -4,6 +4,7 @@ namespace Tdameron1\ObjectOriented;
 
 require_once(dirname(__DIR__,) . "AUTO LOAD PATH GOES HERE");
 
+use http\Exception\InvalidArgumentException;
 use Ramsey\Uuid\Uuid;
 
 /**
@@ -13,7 +14,7 @@ use Ramsey\Uuid\Uuid;
  **/
 
 
-
+//**CLASS HERE**//
 class author {
 
 	//properties below
@@ -27,27 +28,85 @@ class author {
 	 * @var string $authorActivationToken
 	 **/
 	private $authorActivationToken;
+	public function getAuthorActivationToken($authorActivationToken) {
+		$this->authorActivationToken = $authorActivationToken;
+	}
 	/**
 	 * the URL used to define the avatar of the user.
 	 * @var string $authorAvatarUrl
 	 **/
-	public $authorAvatarUrl;
+	private $authorAvatarUrl;
+	public function getAuthorAvatarURL($authorAvatarUrl) {
+		$this->authorAvatarUrl;
+	}
 	/**
 	 * the email address of the author
 	 * @var string $authorEmail
 	 */
 	private $authorEmail;
+	public function getAuthorEmail($authorEmail) {
+		$this->authorEmail;
+	}
 	/**
 	 * The password for the author
 	 * @var string $authorHash
 	 */
 	private $authorHash;
+	public function getAuthorHash($authorHash){
+		$this->authorHash;
+	}
 	/**
 	 * The username of the author
 	 * @var string $authorUsername
 	 */
 	private $authorUsername;
+	public function getAuthorUsername($authorUsername){
+		$this->authorUsername;
+		}
+	}
 
+//**CONSTRUCT HERE**//
+	function __construct($authorId, $authorActivationToken, $authorAvatarUrl, $authorEmail, $authorHash, $authorUsername) {
+		$this->authorId = $authorId;
+		$this->authorActivationToken = $authorActivationToken;
+		$this->authorAvatarUrl = $authorAvatarUrl;
+		$this->authorEmail = $authorEmail;
+		$this->authorHash = $authorHash;
+		$this->authorUsername = $authorUsername;
+	}
+//**GETTERS AND SETTERS HERE**//
 
+/**
+ * @param string $newAuthorId
+ * @throws \InvalidArgumentException if the data types are not InvalidArgumentException
+ * @throws \RangeException if the data values are out of bounds (i.e. too long or negative)
+ * @throws \TypeError if data types violate type hints
+ */
+public function setAuthorId($newAuthorId) : void {
+	try {
+		$uuid = self::validateUuid($newAuthorId);
+	} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+		$exceptionType = get_class($exception);
+		throw(new $exceptionType($exception->getMessage(), 0, $exception));
+	}
 
+	// convert and store the author id
+	$this->authorId = $uuid;
+
+public function setAuthorActivationToken(?string $newAuthorActivationToken): void {
+	if($newAuthorActivationToken ===null) {
+		$this->authorActivationToken === null; {
+		return;
+	}
+	$newAuthorActivationToken = strtolower(trim($newAuthorActivationToken));
+	if(ctype_xdigit($newAuthorActivationToken) === false) {
+		throw(new\RangeException("user activation is not valid"));
+	}
+	//make sure user activation token is only 32 characters
+	if(strlen($newAuthorActivationToken) !== 32) {
+		throw(new\RangeException("user activation token has to be 32"));
+	}
+	$this->authorActivationToken = $newAuthorActivationToken;
 }
+
+
